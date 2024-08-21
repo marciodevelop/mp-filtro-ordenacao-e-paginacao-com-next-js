b533bbd109eaa9cc075e7d3c19ffa4e3b4cf17b5
Marcio <marciocere984@gmail.com>
2024-08-21 00:16:50 -0300

    fix/develop: adding custom hook use-deboun for search input

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -2,15 +2,20 @@
 import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useDebounce } from "@/hooks/use-debounce";
 
 export default function SearchInput() {
   const searchParams = useSearchParams();
   const pathName = usePathname();
   const { replace } = useRouter();
 
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const searchString = event.currentTarget.value;
+  const debouncedSearch = useDebounce(handleSearchResult, 1000);
 
+  function handleChangeInput(event: React.ChangeEvent<HTMLInputElement>) {
+    debouncedSearch(event.currentTarget.value);
+  }
+
+  function handleSearchResult(searchString: string) {
     const params = new URLSearchParams(searchParams);
 
     if (searchString) {
@@ -21,6 +26,7 @@ export default function SearchInput() {
 
     replace(`${pathName}?${params.toString()}`);
   }
+
   return (
     <div className="relative">
       <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
@@ -28,7 +34,7 @@ export default function SearchInput() {
         type="search"
         placeholder="Busque por nome..."
         className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-        onChange={handleChange}
+        onChange={handleChangeInput}
       />
     </div>
   );
---
+++ hooks/use-debounce.tsx
@@ -0,0 +1,20 @@
+import { useRef } from "react";
+
+export function useDebounce<T extends (...args: any[]) => void>(
+  callback: T,
+  delay: number
+) {
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  const debouncedFunction = (...args: Parameters<T>) => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+
+  return debouncedFunction;
+}
